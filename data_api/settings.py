9287620c941717de0b5c7e65d4cc4f435c552b06
Georgi <georgi.manev@entra.energy>
2025-10-13 16:21:13 +0300

    first commit

--- data_api/settings.py
+++ data_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv('DJANGO_SECRET')
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
-SECRET_KEY = 'sdgsdkgmsgms'
+#SECRET_KEY = 'sdgsdkgmsgms'
 
 if not SECRET_KEY:
     raise RuntimeError("DJANGO_SECRET is required")  
