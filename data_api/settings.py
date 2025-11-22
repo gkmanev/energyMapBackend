cf6a9ee05c32f868ae4aa16936edf53a6516fe6b
Georgi <georgi.manev@entra.energy>
2025-11-22 12:14:11 +0200

    price command test

--- data_api/settings.py
+++ data_api/settings.py
@@ -151,7 +151,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     "fetch_prices_hourly": {
         "task": "entsoe_api.tasks.fetch_prices_hourly_task",
-        "schedule": crontab(hour=12, minute=1),
+        "schedule": crontab(hour=12, minute=15),
     },
 }
 
